32b084891147916b616b38122abef1e65d6cd29b
leon macleod <leondeanmacleod@gmail.com>
2019-06-21 12:31:40 +1200

    database is now configured properly with models.py
    Test user created as well.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask;
 from config import Config;
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate;
 
 
 app = Flask(__name__)
 #accessing configuration object from config.py
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-from app import routes
+if __name__ == '__main__':
+    db.create_all()
+
+
+from app import routes, models
+
