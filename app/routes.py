2e27dcc0cf0a5bd708e86b2c77f956920b5886ca
leon macleod <leondeanmacleod@gmail.com>
2019-06-24 14:40:17 +1200

    Many additions, no login form to data implementation yet.

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 
-@app.route('/')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = LoginForm()
 
-@app.route('/index')
-def index():
-    user = {'username':'Leon'}
-    return render_template('index.html', title='Data', user=user);
+    if form.validate_on_submit():
+        flash('Login requested for user{}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('signup.html', title='Sign Up', form=form)
