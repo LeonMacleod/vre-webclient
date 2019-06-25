a9fa32f0de90e9e30c9ae4e9522d496106db6396
leon macleod <leondeanmacleod@gmail.com>
2019-06-26 09:47:38 +1200

    Added error messages to signup form.

--- app/routes.py
+++ app/routes.py
@@ -10,4 +10,17 @@ def signup():
     if form.validate_on_submit():
         flash('Login requested for user{}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
+
+
+        
+
+
+
+
+
     return render_template('signup.html', title='Sign Up', form=form)
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
