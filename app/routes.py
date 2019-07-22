4c055a803569833342a03259e639fc124e109cc0
leon macleod <leondeanmacleod@gmail.com>
2019-07-22 14:23:24 +1200

    Login system is now more robust and only allows user to go to their own page.

--- app/routes.py
+++ app/routes.py
@@ -34,14 +34,14 @@ def Login():
     form = LoginForm()
 
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('user', username=current_user.username))
     
     if form.validate_on_submit():
         user = Users.query.filter_by(username=form.username_or_email.data).first()
         if user is None or not Users.query.filter_by(password=form.password.data).first():
             return redirect(url_for('index'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('user', username=current_user.username))
 
     return render_template('login.html', title='Login', form=form)
         
@@ -56,8 +56,15 @@ def logout():
 @app.route('/user/<username>')
 @login_required
 def user(username):
+    #Only allow users to see their users page
     user = Users.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)  
+    if(current_user.get_id() == user.get_id()):
+        return render_template('user.html', user=user)  
+    else:
+
+        return render_template('index.html')
+
+    
 
     
 
